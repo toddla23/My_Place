961973f74c24b6ee047c853d98ea8c12b5fa762a
Park Jonghyeok <84115021+toddla23@users.noreply.github.com>
2022-10-05 18:29:23 +0900

    Add files via upload

---
+++ FileProsessing.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+
+typedef struct node
+{
+    int key, hight;
+    node* right, *left;
+
+}node;
+
+void initNode(node* n)
+{
+    n->right = NULL;
+    n->left = NULL;
+    n->hight = 1;
+}
+
+void insertNode(node *root, int key)
+{
+    stack<int> s;
+    node* n = new node;
+
+    node * p = root;
+    
+        
+
+    
+}
+
+
+int main(void)
+{
+    node * root = new node;
+
+    return 0;
+}

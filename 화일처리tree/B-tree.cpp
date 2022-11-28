20cc89a2933046e1669c52603fbca188e270c854
DESKTOP-PQ8SB5P\qkrwh <qkrwhdgur0319@gmail.com>
2022-11-28 15:36:45 +0900

    B-tree 수도코드 작성중 마저 작성하고 코드 실행 확인 필요

--- 화일처리tree/B-tree.cpp
+++ 화일처리tree/B-tree.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <cmath>
 
 
 using namespace std;
@@ -14,7 +15,62 @@ typedef struct node
 
 }node;
 
+
 node* getNode
+bool searchPath(node* T, int m, int key, stack &s);
+void insertKey(node* T, int m,node* x, node* y, int newKey);
+int splitNode(node* T, int m, node* x, node* y, int newKey);
+
+void insertBT(node* T, int m, int newKey)
+{
+    if(T == NULL)
+    {
+        T = getNode();
+        T->K.push_back(newKey);
+        T->n = 1;
+        return;
+    }
+    
+    stack<node*> s;
+
+    bool found = searchPath(T, m, newKey, NULL);
+    if(found == true)
+        return;
+    
+    bool finished = false;
+
+    node* x = stack.top();
+    stack.pop();
+    y = null;
+
+    do
+    {
+        if(x.n < m - 1)
+        {
+            insertKey(T, m, x, y, newKey);
+            finished = true;
+        }
+        else
+        {
+            newKey, y = splitNode(T, m, x, y, newKey);
+            if(!s.empty())
+            {    x = s.top(x);
+                s.pop();
+            }
+            else
+            {
+                T = getNode();
+                T.k[1] = newKey;
+                T.p[0] = x;
+                T.p[1] = y;
+                finished = true;
+            }
+
+        }
+
+    }while(!finished);
+
+}
 
 bool searchPath(node* T, int m, int key, stack &s)
 {
@@ -87,76 +143,153 @@ int splitNode(node* T, int m, node* x, node* y, int newKey)
     return centerkey, newnode
 }
 
-void insertBT(node* T, int m, int newKey)
+void deletekey(node* T, int m, node* x, int oldKey);
+int bestSibling(node* T, int m, node* x, node* y);
+void redistributeKeys(node* T, int m, node* x, node* y, int bestSibling);
+
+
+void deleteBT(node* T, int m, int oldKey)
 {
-    if(T == NULL)
-    {
-        T = getNode();
-        T->K.push_back(newKey);
-        T->n = 1;
-        return;
-    }
-    
     stack<node*> s;
+    bool found = searchPath(T, m, oldKey, s);
 
-    bool found = searchPath(T, m, newKey, NULL);
-    if(found == true)
+    if(found == false)
         return;
-    
-    bool finished = false;
 
-    node* x = stack.top();
-    stack.pop();
-    y = null;
+    node* x = s.top();
+    s.pop();
+
+    if(s.size() < T.height()) // old key가 단말노드에 없음
+    {
+        int i;
+        node* internalNode = x;
+
+        for(int n = 0; n < T.n; n++)
+            if(T.k[n] == oldKey)
+            {
+                i = n;
+                break;
+            }
+
+        bool found2 = searchPath(x->P[i], m, x->K[i], s);
+
+        x = s.top();
+        s.pop();
+        node* temp = internalNode->K[i];
+        internalNode.K[i] = x->K[1];
+        x->K[1] = temp;
+        
+    }
+    bool finished = false;
+    deleteKey(T, m, x, oldKey);
+    node* y;
+
+    if(!s.empty())
+    {
+        y = s.top();
+        s.pop();
+    }
 
     do
     {
-        if(x.n < m - 1)
-        {
-            insertKey(T, m, x, y, newKey);
+        if(isRoot(x) || x->n >= round(m / 2) - 1)
             finished = true;
-        }
         else
         {
-            newKey, y = splitNode(T, m, x, y, newKey);
-            if(!s.empty())
-            {    x = s.top(x);
-                s.pop();
+            int bestSibling = bestSibling(T, m, x, y);
+
+            if(y.P[bestSibling].n > round(m/2) - 1)
+            {
+                redistributeKeys(T, m, x, y, bestSibling);
+                finished = true;
+
             }
             else
             {
-                T = getNode();
-                T.k[1] = newKey;
-                T.p[0] = x;
-                T.p[1] = y;
-                finished = true;
+                mergeNode(T, m, x, y, bestSibling);
+                x = y;
+                if(!s.empty())
+                {
+                    y = s.top();
+                    s.pop();
+                }
+                else
+                    finished = true;
             }
 
         }
 
     }while(!finished);
 
-}
-
-deleteBT(node* T, int m, int oldKey)
-{
-    stack<node*> s;
-    bool found, s = searchPath(T, m, oldKey, NULL);
-
-    if(fount == false)
-        return;
-
-    x = s.top();
-    s.pop();
-
-    if(oldKey is not in terminal node)
+    if(y->n == 0)
     {
-        node* internalNode = x;
-        i = 
+        T = y->P[0];
+        discard y node;
     }
 
+}
+
+void deletekey(node* T, int m, node* x, int oldKey)
+{
+    i = 1;
+    while(oldKey > x->K[i])
+        i = i + 1;
+    
+    while (i <= x.n)
+    {
+        x.K[i] = x->K[i+1];
+        x->P[i] = x->P[i+1];
+        i = i + 1;
+    }
+    x->n = x->n -1;
 
 }
+
+int bestSibling(node* T, int m, node* x, node* y);
+{
+    int i = 0;
+    while(y->P[i] != x)
+        i = i + 1;
+    int bestSibling;
+
+    if(i = 0)
+        bestSibling = i + 1;
+    else if(i == y->n)
+        bestSibling = i - 1;
+    else if(y->P[i-1].n >= y.P[i+1].n)
+        bestSibling = i - 1;
+    else
+        bestSibling = i + 1
+
+    return bestSibling;
+}
+
+void redistributeKeys(node* T, int m, node* x, node* y, int bestSibling)
+{
+    int i = 0;
+    while(y->P[i] != x)
+        i = i + 1;
+    
+    node* bestNode = y->P[bestSibling];
+
+    if(bestSibling < i)
+    {
+        int lastKey = bestNode.K[bestNode.n];
+        insertKey(T, m, null, y->K[i]);
+        deletekey(T, m, bestNode, lastKey);
+        y->K[i] = lastKey;
+    }
+    else
+    {
+        int firstKey = bestNode->K[1];
+        insertKey(T, m, x, null, y->K[i+1]);
+        deletekey(T, m, bestNode, firstKey);
+        y->K[i+1] = firstKey;
+    }
+
+}
+
+
 
 
 int main(void)
